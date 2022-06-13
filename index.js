1fcfe7bfe8893126ec5f0399a8635cf0bca113ec
Saiyam vaid <99545380+sam12125@users.noreply.github.com>
2022-06-13 18:06:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-const express = require("express");
-const { news , connection } = require("./db");
+const app = require("express")()
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
-
-app.get("/news/get", async (req, res) => {
-
-    // const params = req.query
-    const News = await news.find()
-    return res.json(News)
-    // res.send("Welcome")
-});
-
-app.post("/news/new" , async(req,res)=>{
-    await news.insertMany({...req.body})
-    return res.send("new news added")
-})
-
+app.get("/", (req,res) => 
+    res.send("App working")
+);
 
 const PORT = process.env.PORT || 8080
 
